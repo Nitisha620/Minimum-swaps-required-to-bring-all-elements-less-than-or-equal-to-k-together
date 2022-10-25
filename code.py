c9a5f83007e08868e6be3eace874eaa8de83be4f
Nitesh7085 <47146458+Nitesh7085@users.noreply.github.com>
2022-10-25 10:03:55 +0530

    range return a sequence of numbers, starting from 0 by default.

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
 def minSwap(arr, n, k) : 
 
 	count = 0
-	for i in range(0, n) : 
+	for i in range(n) : 
 		if (arr[i] <= k) : 
 			count = count + 1
 
 	bad = 0
-	for i in range(0, count) : 
+	for i in range(count) : 
 		if (arr[i] > k) : 
 			bad = bad + 1
 
 	ans = bad 
 	j = count 
-	for i in range(0, n) : 
+	for i in range(n) : 
 		
 		if(j == n) : 
 			break
